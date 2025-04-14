3ea7d4b6f47c1067e4d1ed11b3db2a68f27a0fdd
ShaoHans <shaomenghao@hotmail.com>
2025-04-14 22:36:26 +0800

    B04自动调用插件方法_FunctionChoiceBehavior

---
+++ AIRouter.Console/Plugins/B04自动调用插件方法_FunctionChoiceBehavior.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+namespace AIRouter.Console.Plugins;
+
+internal class B04自动调用插件方法_FunctionChoiceBehavior
+{
+    public static async Task TestAsync(Kernel kernel)
+    {
+        kernel.Plugins.AddFromType<MyMathPlugin>();
+        var settings = new OpenAIPromptExecutionSettings
+        {
+            FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+        };
+
+        System.Console.WriteLine("你可以让AI助理做算术运算");
+        var prompt = System.Console.ReadLine();
+        var result = await kernel
+            .GetRequiredService<IChatCompletionService>()
+            .GetChatMessageContentAsync(
+                prompt: prompt!,
+                executionSettings: settings,
+                kernel: kernel
+            );
+
+        System.Console.WriteLine(result.ToString());
+    }
+}
--- AIRouter.Console/Plugins/MyMathPlugin.cs
+++ AIRouter.Console/Plugins/MyMathPlugin.cs
@@ -10,4 +10,26 @@ internal class MyMathPlugin
     {
         return a + b;
     }
+
+    [KernelFunction, Description("两个数相减")]
+    public static int Subtract([Description("第一个数")] int a, [Description("第二个数")] int b)
+    {
+        return a - b;
+    }
+
+    [KernelFunction, Description("两个数相乘")]
+    public static int Multiply([Description("第一个数")] int a, [Description("第二个数")] int b)
+    {
+        return a * b;
+    }
+
+    [KernelFunction, Description("两个数相除")]
+    public static double Divide([Description("第一个数")] int a, [Description("第二个数")] int b)
+    {
+        if (b == 0)
+        {
+            throw new DivideByZeroException("除数不能为零");
+        }
+        return (double)a / b;
+    }
 }
