938f643e017f7028c4861fe8ea0cf04f98dae8b1
ShaoHans <shaomenghao@hotmail.com>
2025-04-20 11:21:19 +0800

    Agent增加插件

--- Samples/AIRouter.Console/04Agents/D01简单问答Agent.cs
+++ Samples/AIRouter.Console/04Agents/D01简单问答Agent.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Agents;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel.Plugins.Core;
 
 namespace AIRouter.Console.Agents;
 
@@ -7,11 +9,20 @@ internal class D01简单问答Agent
 {
     public static async Task TestAsync(Kernel kernel)
     {
+#pragma warning disable SKEXP0050 // 类型仅用于评估，在将来的更新中可能会被更改或删除。取消此诊断以继续。
+        kernel.Plugins.AddFromType<TimePlugin>();
+#pragma warning restore SKEXP0050 // 类型仅用于评估，在将来的更新中可能会被更改或删除。取消此诊断以继续。
         var agent = new ChatCompletionAgent
         {
             Name = "AI问答助手",
             Instructions = "你是一个问答助手，回答用户的问题。",
-            Kernel = kernel
+            Kernel = kernel,
+            Arguments = new KernelArguments(
+                new OpenAIPromptExecutionSettings
+                {
+                    FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+                }
+            )
         };
 
         // Define a thread variable to maintain the conversation context.
@@ -40,7 +51,12 @@ internal class D01简单问答Agent
             //    System.Console.Write($"{response.Message.Content}"); // Stream to console
             //}
 
-            await foreach (AgentResponseItem<ChatMessageContent> response in agent.InvokeAsync(userMessage, thread))
+            await foreach (
+                AgentResponseItem<ChatMessageContent> response in agent.InvokeAsync(
+                    userMessage,
+                    thread
+                )
+            )
             {
                 System.Console.Write(response.Message.Content);
                 thread = (ChatHistoryAgentThread)response.Thread;
